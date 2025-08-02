95dfffcaaa6c42f228d721b148ac1025575a30bc
Rmin-cmd <Rminpishehvar1379@gmail.com>
2025-08-02 17:41:27 +0330

    Init MAP

--- operators/utils.py
+++ operators/utils.py
@@ -1,4 +1,12 @@
-import monkey_patch_csr_matrix
+# 1) Import the private module
+import scipy.sparse.csr as _csr
+
+# 2) Bring in the old csr_matrix
+from scipy.sparse import csr_matrix
+
+# 3) If scipy didn’t define csr_array yet, alias it to csr_matrix
+if not hasattr(_csr, 'csr_array'):
+    _csr.csr_array = csr_matrix
 
 import numpy as np
 
